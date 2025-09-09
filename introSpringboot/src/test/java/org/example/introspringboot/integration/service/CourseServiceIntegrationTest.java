5cbf2ed9b9714103974ce190b2cce8e21b309907
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2025-09-09 15:59:16 -0500

    Added the first file for integration tests

---
+++ introSpringboot/src/test/java/org/example/introspringboot/integration/service/CourseServiceIntegrationTest.java
@@ -0,0 +1,23 @@
+package org.example.introspringboot.integration.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CourseServiceIntegrationTest {
+
+    @BeforeEach
+    public void setup(){
+
+    }
+
+    //Tests happen after the setups and after every test everything must be emptied
+
+    @AfterEach
+    public void cleanUp(){
+
+    }
+
+
+}
